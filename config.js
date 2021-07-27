06901e43f999ed0764c17bc4081520099bf0db2b
cham_why <53082717+chamwhy@users.noreply.github.com>
2021-07-27 23:25:15 +0900

    update

---
+++ config.js
@@ -0,0 +1,38 @@
+
+const config = 
+{
+    "creator": "chamwhy",
+    "todo": [
+
+    ],
+    "projects": [
+        {
+            "name": "dovv",
+            "type": {
+                "text": "unity",
+                "img": "./img/unity.png"
+            },
+            "description": "탑뷰 로그라이트 게임 제작 중입니다",
+            "img": "./img/dovv.png"
+        },
+        {
+            "name": "decac",
+            "type": {
+                "text": "unity",
+                "img": "./img/unity.png"
+            },
+            "description": "안드로이드 용 간단한 터치게임 제작 중입니다",
+            "img": "./img/decac.png"
+        },
+        {
+            "name": "frect",
+            "type": {
+                "text": "nodejs",
+                "img": "./img/nodejs.png"
+            },
+            "description": "프리랜서들을 위한 커뮤니티 제작중입니다",
+            "img": "./img/frect.png"
+        }
+    ]
+};
+
--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-
+import config from "../config.js";
 (()=>{
     const startView = document.querySelector("#startView");
     const chamwhyBtn = document.querySelector("#chamwhy_icon");
     chamwhyBtn.addEventListener("click", ()=>{
         startView.className += " off";
     });
+})();
+
+(()=>{
+    const body = document.querySelector('#body');
+    const projectHtml = document.querySelector('link[rel="import"]').import.querySelector('.projectBox');
+    console.log(config);
+    for(let i in config.projects){
+        projectHtml.querySelector('.projectName').textContext = i.name;
+    }
 })();
