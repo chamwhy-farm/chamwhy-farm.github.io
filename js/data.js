c23781869c1eb521857b6f5fa85c8621c94beda9
cham_why <53082717+chamwhy@users.noreply.github.com>
2021-08-02 17:35:29 +0900

    update

---
+++ js/data.js
@@ -0,0 +1,20 @@
+const data = {
+    count: 2,
+    projects: [
+        {
+            name: "choco",
+            description: "특정 서버 대상으로 만든 디코봇. 현재 작동하지 않음. open source",
+            thumnail: "choco.png",
+            link: "https://github.com/chamwhy-farm/choco",
+            completed: true
+        },
+        {
+            name: "desktop-todo",
+            description: "바탕화면에서 조작가능한 todo 프로그램",
+            thumnail: "todo.png",
+            link: "private",
+            completed: false
+        }
+    ]
+
+}
--- js/index.js
+++ js/index.js
@@ -1,13 +1,43 @@
+const projectHTML = 
+
 (()=>{
     const projectBtn = document.querySelector('#projectBtn');
     const projectMenu = document.querySelector('#projectMenu');
+    
     const infoBtn = document.querySelector('#infoBtn');
-    const infoMenu = document.querySelector('#infoMenu');
+    const infoMenu = document.querySelector('#wiseBlock');
+
+    const wiseBlock = document.querySelector('#wiseBlock');
+    const wiseSaying1 = wiseBlock.childNodes[1];
+    const wiseSaying2 = wiseBlock.childNodes[3];
+
+
 
     projectBtn.addEventListener('click', function(){
         window.scrollTo({top: projectMenu.offsetTop-30, behavior: 'smooth'})
     });
     infoBtn.addEventListener('click', function(){
-        window.scrollTo({top: infoMenu.offsetTop-30, behavior: 'smooth'})
+        window.scrollTo({top: infoMenu.offsetTop, behavior: 'smooth'})
     });
+    wiseBlock.addEventListener('click', function(){
+        console.log(wiseBlock.childNodes);
+        if(wiseSaying1.classList.contains("hideSaying")){
+            wiseSaying1.classList.remove("hideSaying");
+        }else{
+            wiseSaying1.classList.add("hideSaying");
+        }
+
+        if(wiseSaying2.classList.contains("hideSaying")){
+            wiseSaying2.classList.remove("hideSaying");
+        }else{
+            wiseSaying2.classList.add("hideSaying");
+        }
+
+    });
+
+    data.projects.forEach(project => {
+
+    });
+
 })();
+
